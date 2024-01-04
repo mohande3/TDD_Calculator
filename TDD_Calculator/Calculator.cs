80a4b96e732e2b1e9f0c4ffc12b407e6763130fa
Mohande3Khademi <Mohande3Khademi@hotmail.com>
2024-01-04 11:50:55 +0330

    Add project files.

---
+++ TDD_Calculator/Calculator.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+namespace TDD_Calculator
+{
+    internal class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        public object Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        internal object Mul(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        internal object Sub(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+    }
+}
---
+++ TDD_Calculator/CalculatorTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+
+namespace TDD_Calculator
+{
+    public class CalculatorTest
+    {
+
+        //todo Step1 : Weite Faild test
+        //todo Step2 : Write some code for pass test
+        //todo Step3 : Refactor
+
+        [Fact]
+        public void Test_Add_Method()
+        {
+            var _sut = new Calculator();
+
+            var result = _sut.Add(1, 2);
+
+            result.Should().Be(3);
+        }
+
+
+        [Fact]
+        public void Test_Sub_Method()
+        {
+            var _sut = new Calculator();
+
+            var result = _sut.Sub(1, 2);
+
+            result.Should().Be(-1);
+        }
+
+        [Fact]
+        public void Test_Mul_Method()
+        {
+            var _sut = new Calculator();
+
+            var result = _sut.Mul(1, 2);
+
+            result.Should().Be(2);
+        }
+    }
+}
